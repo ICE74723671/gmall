cdc6c42646bd38cf0dfdba0a881f9a2d672f30ab
ICE <zhaozhi0094@163.com>
2021-03-07 23:01:24 +0800

    初始化项目

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/SpuAttrValueMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.SpuAttrValueEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * spu属性值
+ * 
+ * @author fengge
+ * @email fengge@atguigu.com
+ * @date 2021-03-07 10:50:50
+ */
+@Mapper
+public interface SpuAttrValueMapper extends BaseMapper<SpuAttrValueEntity> {
+	
+}
